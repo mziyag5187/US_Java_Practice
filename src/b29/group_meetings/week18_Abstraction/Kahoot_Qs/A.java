064f287f19738e72ae1d0aa86daa1c3fd1eb0515
zack <mziyag5187@gmail.com>
2023-06-14 21:39:23 +0300

    commit week18 - 06.14

---
+++ src/b29/group_meetings/week18_Abstraction/Kahoot_Qs/A.java
@@ -0,0 +1,27 @@
+package b29.group_meetings.week18_Abstraction.Kahoot_Qs;
+
+public abstract class A {
+     abstract void m1();
+     void m2(){
+         System.out.println("One");
+     }
+}
+
+class B extends A {
+
+    @Override
+    void m1() {
+
+    }
+
+    void m2(){
+        System.out.println("Two");
+    }
+}
+
+class Test{
+    public static void main(String[] args) {
+        B b = new B();
+        b.m2();
+    }
+}
